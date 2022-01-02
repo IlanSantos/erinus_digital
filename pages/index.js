3f90ff8b73fccfaf54e98deb7f40ba47bc19880d
Ilan Santos <ilansalvador@hotmail.com>
2022-01-02 11:57:11 -0300

    Corrigido paragrafos em indice

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
                     <section className="section-presentation mb-4">
                         <Container>
                             <h1>Desenvolvimento de aplicativos e <br /> sistemas para sua empresa</h1>
-                            <p>Descomplique suas tarefas com sistemas ou aplicativos feitos sob medida para você.</p>
+                            <p className="description">Descomplique suas tarefas com sistemas ou aplicativos feitos sob medida para você.</p>
                         </Container>
                     </section>
                     <section >
@@ -21,8 +21,8 @@ export default function Home() {
                             <Row className="g-4 justify-content-between align-items-stretch text-center">
                                 <Col lg="6" md="6" sm="12">
                                     <Card>
-                                        <h2>Sobre nós</h2>
-                                        <p>
+                                        <h2 className="title">Sobre nós</h2>
+                                        <p className="fs-5">
                                             Somos uma startup fundada em 31/12/2021 sediada em Salvador, Bahia.
                                             {' '}<Link href="/sobre"><a>Clique aqui</a></Link> para saber mais.
                                         </p>
@@ -30,8 +30,8 @@ export default function Home() {
                                 </Col>
                                 <Col lg="6" md="6" sm="12">
                                     <Card>
-                                        <h2>Nosso foco</h2>
-                                        <p>
+                                        <h2 className="title">Nosso foco</h2>
+                                        <p className="fs-5">
                                             Nosso objetivo é ajudar pequenas empresas a facilitar seus processos construindo
                                             aplicações/sistemas com o melhor custo x benefício do mercado.
                                         </p>
