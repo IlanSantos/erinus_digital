5764453adf2975b9d8a9c5e8589afda09027ac54
Ilan Santos <ilansalvador@hotmail.com>
2022-01-02 00:36:23 -0300

    Criado página de contato

--- pages/contato.js
+++ pages/contato.js
@@ -1,7 +1,16 @@
 export default function Contato(){
     return (
-        <>
-        
-        </>
+        <main>
+            <section className="container mt-4">
+                <h2 className="title">Contato</h2>
+                <p className="description">Segue abaixo meios de entrar em contato conosco.</p>
+                <div className="mt-4">
+                    <h3 className="title">E-mail</h3>
+                    <p className="fs-5">ilansantos68@gmail.com</p>
+                    <h3>Telefone</h3>
+                    <p className="fs-5">(71) 98169-2388</p>
+                </div>
+            </section>
+        </main>
     )
 }
